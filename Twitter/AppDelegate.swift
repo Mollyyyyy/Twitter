21a5a90af5c3a7c5a448d11331e8d0b0277f625b
Mollyyyyy <apple@appletekiair-4.dynamic.ucsd.edu>
2017-02-21 15:49:46 -0800

    set up got exception

--- Twitter/AppDelegate.swift
+++ Twitter/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import BDBOAuth1Manager
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -39,6 +39,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    @nonobjc func application(app:UIApplication,openURL url:URL,option:[String:
+        AnyObject]) -> Bool{
+        print(url.description)
+        let requestToken = BDBOAuth1Credential(queryString: url.query)
+        return true;
+    
     }
 
 
