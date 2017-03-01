c5588da086592c9952258abff253d75cb2f2ac3b
Mollyyyyy <apple@AppletekiMacBook-Air-4.local>
2017-02-28 21:48:59 -0800

    unresolved

--- Twitter/Tweet.swift
+++ Twitter/Tweet.swift
@@ -18,24 +18,26 @@ class Tweet: NSObject {
     var favoritesCount: Int = 0
     var profileUrl: URL?
     var tagline:String?
+    var tweetDict: NSDictionary!
     init(dictionary: NSDictionary){
-        id = (dictionary["id"] as? Int)!
-        name = dictionary["name"] as? String
-        screenname = dictionary["screen_name"] as? String
-        let profileUrlSting = dictionary["profile_image_url_https"] as? String
-        if let profileUrlSting = profileUrlSting{
-            profileUrl = URL(string: profileUrlSting)
-        }
-        tagline = dictionary["description"] as? String
-
-        text = dictionary["text"] as? String
-        retweetCount = (dictionary["retweet_count"] as? Int) ?? 0
-        favoritesCount = (dictionary["favorites_count"] as? Int) ?? 0
-        let timestampString = dictionary["created_at"] as? String
-        if let timestampString = timestampString{
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss ZZZ"
-            timestamp = formatter.date(from: timestampString)
+        if let dictionary = dictionary["user"] as? NSDictionary{
+            id = (dictionary["id"] as? Int)!
+            name = dictionary["name"] as? String
+            screenname = dictionary["screen_name"] as? String
+            let profileUrlSting = dictionary["profile_image_url_https"] as? String
+            if let profileUrlSting = profileUrlSting{
+                profileUrl = URL(string: profileUrlSting)
+            }
+            tagline = dictionary["description"] as? String
+            text = dictionary["text"] as? String
+            retweetCount = (dictionary["retweet_count"] as? Int) ?? 0
+            favoritesCount = (dictionary["favorites_count"] as? Int) ?? 0
+            let timestampString = dictionary["created_at"] as? String
+            if let timestampString = timestampString{
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd HH:mm:ss ZZZ"
+                timestamp = formatter.date(from: timestampString)
+            }
         }
     }
     class func tweetsWithArray(dictionaries:[NSDictionary]) -> [Tweet]{
@@ -46,5 +48,5 @@ class Tweet: NSObject {
         }
         return tweets
     }
-
+    
 }
